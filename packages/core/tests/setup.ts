7e7237ab3d1e3422e20e2401e1a124da057a321f
liaojie1314 <1517438366@qq.com>
2026-08-10 16:58:09 +0800

    fix(config): load vitest dom matchers

--- packages/core/tests/setup.ts
+++ packages/core/tests/setup.ts
@@ -1,4 +1,4 @@
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom/vitest'
 import { cleanup } from '@testing-library/react'
 import { afterEach, beforeAll, afterAll } from 'vitest'
 import { server } from './mocks/server.js'
